5c3aaab0a93b5b1727c5919831cb99f151a74c96
michael-kelleher <90526300+michael-kelleher@users.noreply.github.com>
2022-10-03 19:51:24 -0400

    Create line.sh

---
+++ Lab 3/line.sh
@@ -0,0 +1,9 @@
+espeak -ven+f2 -k5 -s150 --stdout "Welcome to What's My Line" | aplay
+
+#arecord -f cd -r 441000 -d 5 -t wav recorded.wav && sox recorded.wav recorded_mono.wav remix 1,2
+
+while true:
+do
+arecord -D hw:1,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
+python3 prompt.py recorded_mono.wav
+done
