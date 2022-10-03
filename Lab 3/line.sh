c92c5374bf627931fa92ea51671084613a9d6443
michael-kelleher <90526300+michael-kelleher@users.noreply.github.com>
2022-10-03 19:54:11 -0400

    Update line.sh

--- Lab 3/line.sh
+++ Lab 3/line.sh
@@ -5,5 +5,8 @@ espeak -ven+f2 -k5 -s150 --stdout "Welcome to What's My Line" | aplay
 while true:
 do
 arecord -D hw:1,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
-python3 prompt.py recorded_mono.wav
+res = python3 prompt.py recorded_mono.wav
+read input
+espeak -ven+f2 -k5 -s150 --stdout input | aplay
+
 done
